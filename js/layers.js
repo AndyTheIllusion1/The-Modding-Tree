6c7040c52573a9eba41b9a982fa69d3afc77038c
AndyTheIllusion <32530555+AndyRosin@users.noreply.github.com>
2026-04-05 23:14:18 -0300

    added the buttons for all 12 aspects

--- js/layers.js
+++ js/layers.js
@@ -47,6 +47,266 @@ addNode("1", {
         player.tUnlocked[1] = 1
     }
 })
+addNode("2", {
+    symbol: "<img src='data/blood.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>",
+    color: '#3e1601',
+    position: 2,
+    row: 0,
+    branches: ["Bl"],
+    tooltip: "Unlocks the Blood Aspect",
+    layerShown() {
+        if(player.tUnlocked[2] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+    },
+    canClick() {
+        if(player.tUnlocked[2] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+},
+    onClick() {
+        player.startAspects = player.startAspects+1
+        player.tUnlocked[2] = 1
+    }
+})
+addNode("3", {
+    symbol: "<img src='data/breath.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>",
+    color: '#4379e6',
+    position: 3,
+    row: 0,
+    branches: ["Br"],
+    tooltip: "Unlocks the Breath Aspect",
+    layerShown() {
+        if(player.tUnlocked[3] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+    },
+    canClick() {
+        if(player.tUnlocked[3] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+},
+    onClick() {
+        player.startAspects = player.startAspects+1
+        player.tUnlocked[3] = 1
+    }
+})
+addNode("4", {
+    symbol: "<img src='data/heart.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>",
+    color: '#55142a',
+    position: 4,
+    row: 0,
+    branches: ["Br"],
+    tooltip: "Unlocks the Heart Aspect",
+    layerShown() {
+        if(player.tUnlocked[4] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+    },
+    canClick() {
+        if(player.tUnlocked[4] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+},
+    onClick() {
+        player.startAspects = player.startAspects+1
+        player.tUnlocked[4] = 1
+    }
+})
+addNode("5", {
+    symbol: "<img src='data/mind.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>",
+    color: '#00923d',
+    position: 5,
+    row: 0,
+    branches: ["M"],
+    tooltip: "Unlocks the Mind Aspect",
+    layerShown() {
+        if(player.tUnlocked[5] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+    },
+    canClick() {
+        if(player.tUnlocked[5] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+},
+    onClick() {
+        player.startAspects = player.startAspects+1
+        player.tUnlocked[5] = 1
+    }
+})
+addNode("6", {
+    symbol: "<img src='data/Void.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>",
+    color: '#104ea2',
+    position: 0,
+    row: 1,
+    branches: ["V"],
+    tooltip: "Unlocks the Void Aspect",
+    layerShown() {
+        if(player.tUnlocked[6] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+    },
+    canClick() {
+        if(player.tUnlocked[6] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+},
+    onClick() {
+        player.startAspects = player.startAspects+1
+        player.tUnlocked[6] = 1
+    }
+})
+addNode("7", {
+    symbol: "<img src='data/Light.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>",
+    color: '#f0840c',
+    position: 1,
+    row: 1,
+    branches: ["L"],
+    tooltip: "Unlocks the Light Aspect",
+    layerShown() {
+        if(player.tUnlocked[7] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+    },
+    canClick() {
+        if(player.tUnlocked[7] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+},
+    onClick() {
+        player.startAspects = player.startAspects+1
+        player.tUnlocked[7] = 1
+    }
+})
+addNode("8", {
+    symbol: "<img src='data/rage.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>",
+    color: '#520c61',
+    position: 2,
+    row: 1,
+    branches: ["R"],
+    tooltip: "Unlocks the Rage Aspect",
+    layerShown() {
+        if(player.tUnlocked[8] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+    },
+    canClick() {
+        if(player.tUnlocked[8] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+},
+    onClick() {
+        player.startAspects = player.startAspects+1
+        player.tUnlocked[8] = 1
+    }
+})
+addNode("9", {
+    symbol: "<img src='data/hope.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>",
+    color: '#ffde55',
+    position: 3,
+    row: 1,
+    branches: ["R"],
+    tooltip: "Unlocks the Hope Aspect",
+    layerShown() {
+        if(player.tUnlocked[9] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+    },
+    canClick() {
+        if(player.tUnlocked[9] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+},
+    onClick() {
+        player.startAspects = player.startAspects+1
+        player.tUnlocked[9] = 1
+    }
+})
+addNode("10", {
+    symbol: "<img src='data/doom.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>",
+    color: '#306800',
+    position: 4,
+    row: 1,
+    branches: ["D"],
+    tooltip: "Unlocks the Doom Aspect",
+    layerShown() {
+        if(player.tUnlocked[10] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+    },
+    canClick() {
+        if(player.tUnlocked[10] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+},
+    onClick() {
+        player.startAspects = player.startAspects+1
+        player.tUnlocked[10] = 1
+    }
+})
+addNode("11", {
+    symbol: "<img src='data/life.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>",
+    color: '#a49787',
+    position: 5,
+    row: 1,
+    branches: ["Li"],
+    tooltip: "Unlocks the Life Aspect",
+    layerShown() {
+        if(player.tUnlocked[11] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+    },
+    canClick() {
+        if(player.tUnlocked[11] == 1 || player.startAspects >= 4) {
+            return false
+        } else {
+            return true
+        }
+},
+    onClick() {
+        player.startAspects = player.startAspects+1
+        player.tUnlocked[11] = 1
+    }
+})
 addLayer("T", {
     name: "Time", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "<img src='data/time.png' style='width:calc(90% - 2px);height:calc(90% - 2px);margin:5%'></img>", // This appears on the layer's node. Default is the id with the first letter capitalized
@@ -58,7 +318,7 @@ addLayer("T", {
 		points: new Decimal(0),
     }},
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Primal Points", // Name of prestige currency
+    resource: "Time", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
